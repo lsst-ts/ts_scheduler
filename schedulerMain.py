26c0b4b2621f446781c190c4d03a383c49b24263
fdelgado <fdelgado@lsst.org>
2015-10-06 13:23:09 -0700

    topics exchange test

--- schedulerMain.py
+++ schedulerMain.py
@@ -7,27 +7,51 @@ from SALPY_scheduler import *
 sal = SAL_scheduler()
 sal.setDebugLevel(0)
 
-topicTime = scheduler_timeHandlerC()
+topicTime           = scheduler_timeHandlerC()
+topicObservation    = scheduler_observationTestC()
+topicTarget         = scheduler_targetTestC()
+
 sal.salTelemetrySub("scheduler_timeHandler")
+sal.salTelemetrySub("scheduler_observationTest")
+sal.salTelemetryPub("scheduler_targetTest")
 
 measInterval = 1
+
+measCount  = 0
+visitCount = 0
+syncCount  = 0
+
 stime = time.time()
-count = 0
-totalcount = 0
 try:
-	while True:
-		ntime = time.time()
-		dtime = ntime - stime
-		if dtime >= measInterval:
-			rate = float(count)/dtime
-			print("rx %.0f msg/sec total=%i messages" % (rate, totalcount))
-			stime = ntime
-			count = 0
-		scode = sal.getNextSample_timeHandler(topicTime)
-		if scode == 0 and topicTime.timestamp != 0:
-			count += 1
-			totalcount += 1
-			#print("{}".format(datetime.datetime.fromtimestamp(topicTime.timestamp).isoformat()))
+    while True:
+        ntime = time.time()
+        dtime = ntime - stime
+        if dtime >= measInterval:
+            rate = float(measCount)/dtime
+            print("rx %.0f vitis/sec total=%i visits sync=%i" % (rate, visitCount, syncCount))
+            stime = ntime
+            measCount = 0
+
+        scode = sal.getNextSample_timeHandler(topicTime)
+        if scode == 0 and topicTime.timestamp != 0:
+
+            time.sleep(0.030)
+
+            topicTarget.targetId = visitCount
+            topicTarget.fieldId  = 1234
+            topicTarget.filter   = z
+            topicTarget.ra       = 10.0
+            topicTarget.dec      = 30.0
+            topicTarget.angle    = 45.0
+            topicTarget.num_exposures = 2
+            sal.putSample_targetTest(topicTarget)
+
+            scode = sal.getNextSample_observationTest(topicObservation)
+            if scode == 0 and topicObservation.targetId != 0:
+                measCount += 1
+                visitCount += 1
+                if topicTarget.targetId == topicObservation.targetId:
+                    syncCount += 1
 
 except KeyboardInterrupt:
     sal.salShutdown()
