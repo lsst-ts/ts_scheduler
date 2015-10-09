b285da4142cdced20c2732cf909c3320a01cde02
fdelgado <fdelgado@lsst.org>
2015-10-09 14:55:26 -0700

    target class

---
+++ schedulerTarget.py
@@ -0,0 +1,12 @@
+class schedulerTarget (object):
+    def __init__ (self,
+                targetId=0,
+                fieldId=0,
+                filter="",
+                ra=0.0,
+                dec=0.0,
+                angle=0.0,
+                numexp=0,
+                exptimes=[]):
+        return
+
