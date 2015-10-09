d0399fc439c68de1372c495804591989d6c4d993
fdelgado <fdelgado@lsst.org>
2015-10-09 14:52:07 -0700

    updated test

--- schedulerTest.py
+++ schedulerTest.py
@@ -7,21 +7,44 @@ from SALPY_scheduler import *
 sal = SAL_scheduler()
 sal.setDebugLevel(0)
 
-topicTime = scheduler_timeHandlerC()
+topicTime           = scheduler_timeHandlerC()
+topicObservation    = scheduler_observationTestC()
+topicTarget         = scheduler_targetTestC()
+
 sal.salTelemetryPub("scheduler_timeHandler")
+sal.salTelemetryPub("scheduler_observationTest")
+sal.salTelemetrySub("scheduler_targetTest")
 
 start_date = "2010-10-01 16:28:00"
 start_seconds = time.mktime(time.strptime(start_date, "%Y-%m-%d %H:%M:%S"))
 delta_seconds = 40
 
-topicTime.timestamp = int(start_seconds)
+#topicTime.timestamp = int(start_seconds)
+topicTime.timestamp = time.time()
 
 measInterval = 1
 stime = time.time()
 count = 0
 totalcount = 0
+observationId = 0
 try:
     while True:
+        sal.putSample_timeHandler(topicTime)
+
+        while True:
+            scode = sal.getNextSample_targetTest(topicTarget)
+            if scode == 0 and topicTarget.targetId != 0:
+                observationId += 1
+                topicObservation.observationId = observationId
+                topicObservation.targetId      = topicTarget.targetId
+
+#                topicTime.timestamp += delta_seconds
+                topicTime.timestamp = time.time()
+
+                sal.putSample_observationTest(topicObservation)
+
+                break
+
         ntime = time.time()
         dtime = ntime - stime
         if dtime >= measInterval:
@@ -29,12 +52,11 @@ try:
             print("tx %.0f msg/sec total=%i messages" % (rate, totalcount))
             stime = ntime
             count = 0
-        sal.putSample_timeHandler(topicTime)
-        topicTime.timestamp += delta_seconds
         count += 1
         totalcount += 1
-        time.sleep(0.030)
-        #print("{}".format(datetime.datetime.fromtimestamp(topicTime.timestamp).isoformat()))
+        if (totalcount >= 3000):
+            break
+
 except KeyboardInterrupt:
     sal.salShutdown()
     sys.exit(0)
