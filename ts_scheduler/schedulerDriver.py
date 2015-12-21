51c44204de8f27ae8e61d8dd772d9cdd070684f4
fdelgado <fdelgado@lsst.org>
2015-12-21 14:33:53 -0700

    Included fields table.

--- ts_scheduler/schedulerDriver.py
+++ ts_scheduler/schedulerDriver.py
@@ -1,18 +1,47 @@
 import time
 
-from schedulerTarget import *
+from schedulerTarget  import *
 from observatoryModel import *
+from schedulerField   import *
 
 class schedulerDriver (object):
     def __init__ (self, log):
 
-	self.log = log
+        self.log = log
 
-	self.observatoryModel = ObservatoryModel(self.log)
+        self.observatoryModel = ObservatoryModel(self.log)
+        self.buildFieldsTable()
 
         self.time = 0.0
         self.targetId = 0
         self.newTarget = schedulerTarget()
+
+        return
+
+    def buildFieldsTable(self):
+
+        lines = file("../conf/system/tessellationFields").readlines()
+        fieldID = 0
+        fieldsTable = {}
+        for line in lines:
+            line = line.strip()
+            if not line:			# skip blank line
+                continue
+            if line[0]=='#': 		# skip comment line
+                continue
+            fieldID += 1
+            values = line.split()
+            field = schedulerField()
+            field.fieldID = fieldID
+            field.ra_RAD  = eval(values[0])*DEG2RAD
+            field.dec_RAD = eval(values[1])*DEG2RAD
+            field.gl_RAD  = eval(values[2])*DEG2RAD
+            field.gb_RAD  = eval(values[3])*DEG2RAD
+            field.el_RAD  = eval(values[4])*DEG2RAD
+            field.eb_RAD  = eval(values[5])*DEG2RAD
+
+            fieldsTable[fieldID] = field
+            print fieldsTable[fieldID]
 
         return
 
