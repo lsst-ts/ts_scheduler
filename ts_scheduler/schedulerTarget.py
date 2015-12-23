afe07da3eb5f9f47597a037936ef93c09e9e3e7d
fdelgado <fdelgado@lsst.org>
2015-12-23 14:09:32 -0700

    print

--- ts_scheduler/schedulerTarget.py
+++ ts_scheduler/schedulerTarget.py
@@ -1,3 +1,5 @@
+from schedulerDefinitions import *
+
 class schedulerTarget(object):
     def __init__(self,
                 targetId=0,
@@ -22,4 +24,7 @@ class schedulerTarget(object):
         self.value      = 0.0
 
         return
+
+    def __str__(self):
+        return "ID=%d field=%d ra=%.3f dec=%.3f" % (self.targetId, self.fieldId, self.ra_RAD*RAD2DEG, self.dec_RAD*RAD2DEG)
 
