3151280db0aea229522011c18457d77290e18266
fdelgado <fdelgado@lsst.org>
2015-12-23 13:04:25 -0700

    Parent generic class for the proposals.

---
+++ ts_scheduler/schedulerProposal.py
@@ -0,0 +1,32 @@
+from schedulerDefinitions import *
+from schedulerTarget      import *
+
+class schedulerProposal(object):
+    def __init__(self, log, configFilePath):
+
+        self.log = log
+
+        self.propConfigDict, pairs = readConfFile(configFilePath)
+
+        return
+
+    def startSurvey(self):
+
+        return
+
+    def endSurvey(self):
+
+        return
+
+    def startNight(self):
+
+        return
+
+    def endNight(self):
+
+        return
+
+    def suggestTargets(self):
+
+        return
+
