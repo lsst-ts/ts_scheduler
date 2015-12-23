328b6c3e505481d64bf0a8b56b08d4cea7c241b1
fdelgado <fdelgado@lsst.org>
2015-12-23 13:07:57 -0700

    target object defined

--- ts_scheduler/schedulerTarget.py
+++ ts_scheduler/schedulerTarget.py
@@ -1,12 +1,25 @@
-class schedulerTarget (object):
-    def __init__ (self,
+class schedulerTarget(object):
+    def __init__(self,
                 targetId=0,
                 fieldId=0,
                 filter="",
-                ra=0.0,
-                dec=0.0,
-                angle=0.0,
+                ra_RAD=0.0,
+                dec_RAD=0.0,
+                ang_RAD=0.0,
                 numexp=0,
                 exptimes=[]):
+
+        self.targetId = targetId
+        self.fieldId  = fieldId
+        self.ra_RAD   = ra_RAD
+        self.dec_RAD  = dec_RAD
+        self.ang_RAD  = ang_RAD
+        self.numexp   = numexp
+        self.exptimes = list(exptimes)
+
+        self.propIds    = []
+        self.propValues = []
+        self.value      = 0.0
+
         return
 
