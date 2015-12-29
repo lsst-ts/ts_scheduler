813affc3878381b1f693f2e39e52bfd79d4212d5
Michael Reuter <mareuter@lsst.org>
2015-12-29 14:22:22 -0700

    Changing ObservatoryModel configuration.
    Using a new config file mechanism and allowing it to create a standard
    dictionary. Also, I've harmonized names between the topic library, SOCS and
    Scheduler code bases to make the configuration easier.

--- ts_scheduler/schedulerDefinitions.py
+++ ts_scheduler/schedulerDefinitions.py
@@ -19,33 +19,33 @@ def readConfFile(fileName):
     """
     Parse the configuration file (fileName) and return a dictionary of
     the content of the file.
-    
+
     fileName must be an ASCII file containing a list of key = value
     pairs, one pair per line. Comments are identified by a '#' and can
-    be anywhere in the file. Everything following a '#' (up to the 
-    carriage return/new line) is considered to be a comment and 
+    be anywhere in the file. Everything following a '#' (up to the
+    carriage return/new line) is considered to be a comment and
     ignored.
-    
-    The dictionary has the form {key: value} where value is a simple 
-    number if and only if key appears only one time in fileName. 
+
+    The dictionary has the form {key: value} where value is a simple
+    number if and only if key appears only one time in fileName.
     Otherwise, value is an array.
-    
-    Value can have '=' sign in it: each non-comment line is split 
-    using the '=' character as delimiter, only once and starting from 
+
+    Value can have '=' sign in it: each non-comment line is split
+    using the '=' character as delimiter, only once and starting from
     the left. Extra white spaces and '\n' characters are stripped from
     both key and value.
-    
-    An attempt is made to convert value into a float. If that fails, 
+
+    An attempt is made to convert value into a float. If that fails,
     value is assumed to be a string.
-    
-    
+
+
     Input
     fileName:   the name (with path) of the configuration file.
-    
+
     Return
     A dictionary of key = value elements.
     A 2d array of key, value pairs.
-    
+
     Raise
     IOError if fileName cannot be opened for reading.
     """
@@ -53,10 +53,10 @@ def readConfFile(fileName):
     pairs = []
     index = 0
 
-    # Try and read the file fileName (raise IOError if something bad 
+    # Try and read the file fileName (raise IOError if something bad
     # happens).
     lines = file(fileName).readlines ()
-    
+
     for line in lines:
         line = line.strip()
         if not line:			# skip blank line
@@ -87,7 +87,7 @@ def readConfFile(fileName):
             except:
                 # Ops, must be a string, then
                 pass
-            
+
             if not conf.has_key (key):
                 conf[key] = val
             elif (isinstance (conf[key], list)):
@@ -96,3 +96,75 @@ def readConfFile(fileName):
                 conf[key] = [conf[key], val]
     return conf, pairs
 
+def readNewConfFile(filename):
+    """Read the new type of configuration file.
+
+    This function reads the new type of configuration file that contains sections. It also
+    has the capability to take parameters as math expressions and lists. String entries in
+    list parameters do not need to be surrounded by quotes. An example file is shown below:
+
+    [section]
+    # Floating point parameter
+    var1 = 1.0
+    # String parameter
+    var2 = help
+    # List of strings parameter
+    var3 = [good, to, go]
+    # List of floats parameter
+    var4 = [1, 2, 4]
+    # Boolean parameter
+    var5 = True
+    # Floating point math epxression parameter
+    var6 = 375. / 30.
+
+    Parameters
+    ----------
+    filename : str
+               The configuration file name.
+
+    Returns
+    -------
+    dict
+        A dictionary from the configuration file.
+    """
+    import ConfigParser as configparser
+    config = configparser.SafeConfigParser()
+    config.read(filename)
+
+    from collections import defaultdict
+    config_dict = defaultdict(dict)
+    math_ops = "+,-,*,/".split(',')
+
+    for section in config.sections():
+        for key, _ in config.items(section):
+            try:
+                value = config.getboolean(section, key)
+            except ValueError:
+                try:
+                    value = config.getfloat(section, key)
+                except ValueError:
+                    value = config.get(section, key)
+
+                    # Handle parameters with math operations
+                    check_math = [op for op in math_ops if op in value]
+                    if len(check_math):
+                        value = eval(value)
+
+                    try:
+                        # Handle lists from the configuration
+                        if value.startswith('['):
+                            value = value.strip('[]')
+                            try:
+                                value = [float(x) for x in value.split(',')]
+                            except ValueError:
+                                value = [x.strip() for x in value.split(',')]
+                            if len(value) == 1:
+                                if value[0] == '':
+                                    value = []
+                    except AttributeError:
+                        # Above was a float
+                        pass
+
+            config_dict[section][key] = value
+
+    return config_dict
