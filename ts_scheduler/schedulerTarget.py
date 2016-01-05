c3a5b05bc58a4cd9a343d606d38c9cd2d0e84ed8
fdelgado <fdelgado@lsst.org>
2016-01-05 16:33:21 -0700

    lint compliant

--- ts_scheduler/schedulerTarget.py
+++ ts_scheduler/schedulerTarget.py
@@ -1,30 +1,30 @@
-from schedulerDefinitions import *
+from schedulerDefinitions import RAD2DEG
 
 class schedulerTarget(object):
     def __init__(self,
-                targetId=0,
-                fieldId=0,
-                filter="",
-                ra_RAD=0.0,
-                dec_RAD=0.0,
-                ang_RAD=0.0,
-                numexp=0,
-                exptimes=[]):
+                 targetId=0,
+                 fieldId=0,
+                 filter="",
+                 ra_RAD=0.0,
+                 dec_RAD=0.0,
+                 ang_RAD=0.0,
+                 numexp=0,
+                 exptimes=[]):
 
         self.targetId = targetId
-        self.fieldId  = fieldId
-        self.ra_RAD   = ra_RAD
-        self.dec_RAD  = dec_RAD
-        self.ang_RAD  = ang_RAD
-        self.numexp   = numexp
+        self.fieldId = fieldId
+        self.ra_RAD = ra_RAD
+        self.dec_RAD = dec_RAD
+        self.ang_RAD = ang_RAD
+        self.numexp = numexp
         self.exptimes = list(exptimes)
 
-        self.propIds    = []
+        self.propIds = []
         self.propValues = []
-        self.value      = 0.0
+        self.value = 0.0
 
         return
 
     def __str__(self):
-        return "ID=%d field=%d ra=%.3f dec=%.3f" % (self.targetId, self.fieldId, self.ra_RAD*RAD2DEG, self.dec_RAD*RAD2DEG)
-
+        return ("ID=%d field=%d ra=%.3f dec=%.3f" %
+                (self.targetId, self.fieldId, self.ra_RAD * RAD2DEG, self.dec_RAD * RAD2DEG))
