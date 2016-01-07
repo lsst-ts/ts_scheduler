841964a4ca1167b58e29790278a5f14f8eacbf08
fdelgado <fdelgado@lsst.org>
2016-01-07 13:29:51 -0700

    PEP8 compliant

--- ts_scheduler/schedulerField.py
+++ ts_scheduler/schedulerField.py
@@ -1,27 +1,27 @@
 from schedulerDefinitions import RAD2DEG
 
-class schedulerField(object):
+class Field(object):
 
     def __init__(self,
-                 fieldId=0,
-                 ra_RAD=0.0,
-                 dec_RAD=0.0,
-                 gl_RAD=0.0,
-                 gb_RAD=0.0,
-                 el_RAD=0.0,
-                 eb_RAD=0.0,
-                 fov_RAD=0.0):
+                 fieldid=0,
+                 ra_rad=0.0,
+                 dec_rad=0.0,
+                 gl_rad=0.0,
+                 gb_rad=0.0,
+                 el_rad=0.0,
+                 eb_rad=0.0,
+                 fov_rad=0.0):
 
-        self.fieldId = fieldId
-        self.ra_RAD = ra_RAD
-        self.dec_RAD = dec_RAD
-        self.gl_RAD = gl_RAD
-        self.gb_RAD = gb_RAD
-        self.el_RAD = el_RAD
-        self.eb_RAD = eb_RAD
-        self.fov_RAD = fov_RAD
+        self.fieldid = fieldid
+        self.ra_rad = ra_rad
+        self.dec_rad = dec_rad
+        self.gl_rad = gl_rad
+        self.gb_rad = gb_rad
+        self.el_rad = el_rad
+        self.eb_rad = eb_rad
+        self.fov_rad = fov_rad
 
     def __str__(self):
         return ("ID=%d ra=%.3f dec=%.3f gl=%.3f gb=%.3f el=%.3f eb=%.3f fov=%.3f" %
-                (self.fieldId, self.ra_RAD * RAD2DEG, self.dec_RAD * RAD2DEG, self.gl_RAD * RAD2DEG,
-                 self.gb_RAD * RAD2DEG, self.el_RAD * RAD2DEG, self.eb_RAD * RAD2DEG, self.fov_RAD * RAD2DEG))
+                (self.fieldid, self.ra_rad * RAD2DEG, self.dec_rad * RAD2DEG, self.gl_rad * RAD2DEG,
+                 self.gb_rad * RAD2DEG, self.el_rad * RAD2DEG, self.eb_rad * RAD2DEG, self.fov_rad * RAD2DEG))
