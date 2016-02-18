fd7b3cfb73991ee18285c4304402046dc80e10e3
Michael Reuter <mareuter@lsst.org>
2016-02-18 13:46:09 -0700

    Initial cut at getting sky brightness.

--- tests/sky_model/test_astro_sky.py
+++ tests/sky_model/test_astro_sky.py
@@ -1,3 +1,4 @@
+import numpy
 import unittest
 
 from ts_scheduler.sky_model import AstronomicalSkyModel
@@ -12,7 +13,15 @@ class AstronomicalSkyTest(unittest.TestCase):
     def test_basic_information_after_initial_creation(self):
         self.assertIsNotNone(self.astro_sky.date_profile)
         self.assertEqual(self.astro_sky.date_profile.timestamp, 0)
+        self.assertIsNotNone(self.astro_sky.sky_brightness)
 
     def test_update_mechanism(self):
         self.astro_sky.update(LSST_START_TIMESTAMP)
         self.assertEqual(self.astro_sky.date_profile.timestamp, LSST_START_TIMESTAMP)
+
+    def test_sky_brightness_retrieval_internal_time_array_of_positions(self):
+        ra_rads = numpy.radians(numpy.linspace(0., 90., 19))
+        dec_rads = numpy.radians(numpy.linspace(-90., 0., 19))
+        self.astro_sky.update(LSST_START_TIMESTAMP)
+        sky_mags = self.astro_sky.get_sky_brightness(ra_rads, dec_rads)
+        self.assertEqual(sky_mags.size, ra_rads.size * 6)
--- ts_scheduler/sky_model/astro_sky.py
+++ ts_scheduler/sky_model/astro_sky.py
@@ -1,6 +1,8 @@
 from enum import Enum
 import logging
 
+from lsst.sims.skybrightness import SkyModel
+
 from .date_profile import DateProfile
 
 class Planets(Enum):
@@ -31,6 +33,7 @@ class AstronomicalSkyModel(object):
         """
         self.log = logging.getLogger("sky_model.AstronomicalSkyModel")
         self.date_profile = DateProfile(0, location)
+        self.sky_brightness = SkyModel(mags=True)
 
     def update(self, timestamp):
         """Update the internal timestamp.
@@ -41,3 +44,24 @@ class AstronomicalSkyModel(object):
             The UNIX timestamp to update the internal timestamp to.
         """
         self.date_profile.update(timestamp)
+
+    def get_sky_brightness(self, ra, dec):
+        """Get the LSST 6 filter sky brightness for a set of positions at a single time.
+
+        This function retrieves the LSST 6 filter sky brightness magnitudes for a given set
+        of sky positions at the MJD kept by the :class:`.DateProfile.`
+
+        Parameters
+        ----------
+        ra : numpy.ndarray
+            The right ascension values (radians) for the sky positions.
+        dec : numpy.ndarray
+            The declination values (radians) for the sky positions.
+
+        Returns
+        -------
+        numpy.ndarray
+            The LSST 6 filter sky brightness magnitudes.
+        """
+        self.sky_brightness.setRaDecMjd(ra, dec, self.date_profile.mjd)
+        return self.sky_brightness.returnMags()
