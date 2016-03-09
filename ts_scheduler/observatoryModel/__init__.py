6b5a3dcb8b19e986e15ea435c74d45ddfdf6e3df
fdelgado <fdelgado@lsst.org>
2016-03-09 15:00:14 -0700

    split

--- ts_scheduler/observatoryModel/__init__.py
+++ ts_scheduler/observatoryModel/__init__.py
@@ -3,3 +3,8 @@
 __author__ = 'Francisco Delgado'
 __email__ = 'fdelgado@lsst.org'
 __version__ = '0.2.1'
+
+from .observatoryLocation import *
+from .observatoryPosition import *
+from .observatoryState import *
+from .observatoryModel import *
