6aef0a090b4322263a6a41ce3d61130ece9f46b8
Francisco Delgado <fdelgado@lsst.org>
2016-03-22 17:47:57 -0700

    Added reference for sky model

--- ts_scheduler/schedulerProposal.py
+++ ts_scheduler/schedulerProposal.py
@@ -3,11 +3,13 @@ import logging
 from schedulerDefinitions import read_conf_file
 
 class Proposal(object):
-    def __init__(self, configfilepath):
+    def __init__(self, configfilepath, skymodel):
 
         self.log = logging.getLogger("scheduler")
 
         self.proposal_confdict = read_conf_file(configfilepath)
+
+        self.skyModel = skymodel
 
     def start_survey(self):
         return
