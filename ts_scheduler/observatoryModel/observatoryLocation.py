e9982125b4857a5e175b524d06eb3105a71d080c
Francisco Delgado <fdelgado@lsst.org>
2016-03-22 17:45:16 -0700

    Added configure method

--- ts_scheduler/observatoryModel/observatoryLocation.py
+++ ts_scheduler/observatoryModel/observatoryLocation.py
@@ -1,3 +1,5 @@
+import math
+
 class ObservatoryLocation(object):
 
     def __init__(self,
@@ -5,8 +7,14 @@ class ObservatoryLocation(object):
                  longitude_rad=0.0,
                  height=0.0):
         # meters
-        self.Height = height
+        self.height = height
 
         # radians
         self.latitude_rad = latitude_rad
         self.longitude_rad = longitude_rad
+
+    def configure(self, location_confdict):
+
+        self.latitude_rad = math.radians(location_confdict["obs_site"]["latitude"])
+        self.longitude_rad = math.radians(location_confdict["obs_site"]["longitude"])
+        self.height = location_confdict["obs_site"]["height"]
