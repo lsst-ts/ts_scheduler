659ed564b1219fbf9d5e50ea9f5a7eb5dd77c412
Srinivasan Chandrasekharan <srini@lsst.org>
2016-03-31 15:46:36 -0400

    Adding unit test for getting data from Fields.db

---
+++ opsim3.reference/DataForInstall/FieldGen/TestFieldGen.py
@@ -0,0 +1,35 @@
+import unittest
+from AddGalEcl import *
+import numpy
+import sqlite3
+
+class TestFieldGen(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_add_gal_ecl(self):
+        createTessellationFile("input", "output_unittest")
+
+        orig = numpy.loadtxt("tessellationFields_UnitTest")
+        calc = numpy.loadtxt("output_unittest")
+
+        diff = orig - calc
+
+        self.assertEqual(numpy.array_equal(orig, calc), True)
+
+    def test_query_field_table(self):
+        # https://docs.python.org/2/library/sqlite3.html
+        # More examples are here
+
+        conn = sqlite3.connect("Fields.db")
+        cursor = conn.cursor()
+
+        sql = "select fieldID, fieldFov, fieldRA, fieldDEC, fieldGL, fieldGB, fieldEL, fieldEB from Field limit 0,10"
+
+        data = cursor.execute(sql)
+
+        self.assertEqual(data.arraysize>0, True)
+
+if __name__ == "__main__":
+    unittest.main()
