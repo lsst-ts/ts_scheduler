a601b527ca257a28f7cb0c5c78f61aff0352a9bf
Michael Reuter <mareuter@lsst.org>
2016-04-11 09:46:34 -0700

    Fixing deprecated test call.

--- tests/setup/test_log.py
+++ tests/setup/test_log.py
@@ -25,7 +25,7 @@ class LogTest(unittest.TestCase):
         mock_strftime.return_value = "2016-03-10_15:50:01"
         log_path = generate_logfile()
         self.assertEqual(mock_strftime.called, 1)
-        self.assertEquals(os.path.basename(log_path), self.log_file_name)
+        self.assertEqual(os.path.basename(log_path), self.log_file_name)
 
     def test_configure_logging_default(self):
         configure_logging(self.args, self.log_file_name)
