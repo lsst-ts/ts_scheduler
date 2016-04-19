97a0327ace101d6401e42dedbc9bb86a8a72e3f0
Francisco Delgado <fdelgado@lsst.org>
2016-04-19 16:48:35 -0700

    reformated, renamed, and added automatic backup when already existing database file, which makes it also unit-testable.

--- ts_scheduler/fields/createFieldsDB.py
+++ ts_scheduler/fields/createFieldsDB.py
@@ -1,11 +1,18 @@
 import sys
+import os
+import time
 import sqlite3
 
 def printUsage():
     print "Usage: python createFieldsDB.py"
 
-def createFieldsDB() :
-    conn = sqlite3.connect("Fields.db")
+def createFieldsDB(dbfilename = "Fields.db") :
+
+    if os.path.isfile(dbfilename):
+        timestr = time.strftime(".%Y-%m-%d_%H:%M:%S")
+        os.rename(dbfilename, dbfilename + timestr)
+
+    conn = sqlite3.connect(dbfilename)
     conn.close()
 
 if __name__ == "__main__" :
