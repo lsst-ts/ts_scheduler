f02a8a2ceba979421204b04a7ef55b2ade7e4a8c
Francisco Delgado <fdelgado@lsst.org>
2016-04-19 16:49:49 -0700

    reformated and made unit-testable

--- ts_scheduler/fields/createFieldsTable.py
+++ ts_scheduler/fields/createFieldsTable.py
@@ -4,8 +4,8 @@ import sqlite3
 def printUsage():
     print "Usage: python createFieldsTable.py"
 
-def createFieldsTable() :
-    conn = sqlite3.connect("Fields.db")
+def createFieldsTable(dbfilename = "Fields.db") :
+    conn = sqlite3.connect(dbfilename)
     cursor = conn.cursor()
 
     sql = "CREATE TABLE Field(fieldID INT PRIMARY KEY NOT NULL, fieldFov REAL NOT NULL, fieldRA REAL NOT NULL, fieldDEC REAL NOT NULL, fieldGL REAL NOT NULL, fieldGB REAL NOT NULL, fieldEL REAL NOT NULL, fieldEB REAL NOT NULL);"
