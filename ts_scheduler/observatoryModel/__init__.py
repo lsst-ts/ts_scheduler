a66688747da0ab9e649230abc5cc9d409309e2ef
Francisco Delgado <fdelgado@lsst.org>
2016-04-29 17:57:53 -0700

    updated tag to 0.2.6

--- ts_scheduler/observatoryModel/__init__.py
+++ ts_scheduler/observatoryModel/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Francisco Delgado'
 __email__ = 'fdelgado@lsst.org'
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 from .observatoryLocation import *
 from .observatoryPosition import *
