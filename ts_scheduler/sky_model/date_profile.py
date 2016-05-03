2e5e76caa27453813009462ffc50e81370909216
Michael Reuter <mareuter@lsst.org>
2016-05-03 10:38:45 -0700

    Reordering functions in date_profile.

--- ts_scheduler/sky_model/date_profile.py
+++ ts_scheduler/sky_model/date_profile.py
@@ -24,43 +24,6 @@ class DateProfile(object):
         self.location = location
         self.update(timestamp)
 
-    def update(self, timestamp):
-        """Change the internal timestamp to requested one.
-
-        Parameters
-        ----------
-        timestamp : float
-            The UNIX timestamp to update the internal timestamp to.
-        """
-        self.timestamp = timestamp
-        self.current_dt = datetime.utcfromtimestamp(self.timestamp)
-
-    @property
-    def mjd(self):
-        """Return the Modified Julian Date for the internal timestamp.
-
-        Returns
-        -------
-        float
-        """
-        mjd = palpy.caldj(self.current_dt.year, self.current_dt.month, self.current_dt.day)
-        mjd += (self.current_dt.hour / 24.0) + (self.current_dt.minute / 1440.) + \
-               (self.current_dt.second / 86400.)
-        return mjd
-
-    @property
-    def lst_rad(self):
-        """Return the Local Sidereal Time (radians) for the internal timestamp.
-
-        Returns
-        -------
-        float
-        """
-        value = palpy.gmst(self.mjd) + self.location.longitude_rad
-        if value < 0.:
-            value += 2.0 * math.pi
-        return value
-
     def __call__(self, timestamp):
         """Modified Julian Date and Local Sidereal Time from instance.
 
@@ -76,3 +39,40 @@ class DateProfile(object):
         """
         self.update(timestamp)
         return (self.mjd, self.lst_rad)
+
+    @property
+    def lst_rad(self):
+        """Return the Local Sidereal Time (radians) for the internal timestamp.
+
+        Returns
+        -------
+        float
+        """
+        value = palpy.gmst(self.mjd) + self.location.longitude_rad
+        if value < 0.:
+            value += 2.0 * math.pi
+        return value
+
+    @property
+    def mjd(self):
+        """Return the Modified Julian Date for the internal timestamp.
+
+        Returns
+        -------
+        float
+        """
+        mjd = palpy.caldj(self.current_dt.year, self.current_dt.month, self.current_dt.day)
+        mjd += (self.current_dt.hour / 24.0) + (self.current_dt.minute / 1440.) + \
+               (self.current_dt.second / 86400.)
+        return mjd
+
+    def update(self, timestamp):
+        """Change the internal timestamp to requested one.
+
+        Parameters
+        ----------
+        timestamp : float
+            The UNIX timestamp to update the internal timestamp to.
+        """
+        self.timestamp = timestamp
+        self.current_dt = datetime.utcfromtimestamp(self.timestamp)
