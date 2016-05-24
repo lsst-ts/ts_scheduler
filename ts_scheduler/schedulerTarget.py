19178dede1f2184fa4ffd6ac1c86b84656ebbafd
Francisco Delgado <fdelgado@lsst.org>
2016-05-23 19:08:58 -0700

    added airmass added cost_bonus added num_props (number of proposals interested on this target) updated __str__

--- ts_scheduler/schedulerTarget.py
+++ ts_scheduler/schedulerTarget.py
@@ -23,25 +23,31 @@ class Target(object):
         self.exp_times = list(exp_times)
 
         self.time = 0.0
+        self.airmass = 0.0
         self.sky_brightness = 0.0
 
         self.goal = 0
         self.visits = 0
         self.progress = 0.0
 
-        self.propid_list = []
-        self.propvalue_list = []
         self.value = 0.0
+
         self.slewtime = 0.0
-        self.cost = 0.0
+        self.cost_bonus = 0.0
         self.rank = 0.0
 
+        self.num_props = 0
+        self.propid_list = []
+        self.propvalue_list = []
+
     def __str__(self):
-        return ("targetid=%d field=%d filter=%s exp_times=%s ra=%.3f dec=%.3f time=%.1f "
-                "brightness=%.3f progress=%.3f slewtime=%.3f value=%.3f cost=%.3f rank=%.3f propid=%s" %
+        return ("targetid=%d field=%d filter=%s exp_times=%s ra=%.3f dec=%.3f "
+                "time=%.1f airmass=%.3f brightness=%.3f "
+                "progress=%.3f value=%.3f slewtime=%.3f cost=%.3f rank=%.3f propid=%s" %
                 (self.targetid, self.fieldid, self.filter, str(self.exp_times),
-                 self.ra_rad * RAD2DEG, self.dec_rad * RAD2DEG, self.time, self.sky_brightness,
-                 self.progress, self.slewtime, self.value, self.cost, self.rank, self.propid_list))
+                 self.ra_rad * RAD2DEG, self.dec_rad * RAD2DEG,
+                 self.time, self.airmass, self.sky_brightness,
+                 self.progress, self.value, self.slewtime, self.cost_bonus, self.rank, self.propid_list))
 
     @classmethod
     def from_topic(cls, topic):
