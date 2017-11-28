e5ddf2a107d709eaa091ea5a91a9af532ddcb30b
Colin Winslow <cwinslow@lsst.org>
2017-11-28 12:46:01 -0700

    print proposal name along with nightly visits dict

--- python/lsst/ts/scheduler/proposals/area_distribution.py
+++ python/lsst/ts/scheduler/proposals/area_distribution.py
@@ -182,7 +182,10 @@ class AreaDistributionProposal(Proposal):
     def end_night(self, timestamp):
 
         Proposal.end_night(self, timestamp)
+        print(self.name)
         print(self.fieldsvisitedtonight)
+        print()
+        
         self.fieldsvisitedtonight.clear()
         self.log.info("end_night survey fields=%i targets=%i goal=%i visits=%i progress=%.2f%%" %
                       (self.survey_fields, self.survey_targets,
