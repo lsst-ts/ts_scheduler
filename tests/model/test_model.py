4147fa4b3c854d4affbc0e18121eec7b3c555305
Andrew Heyer <aheyer@lsst.org>
2018-10-05 10:53:27 -0700

    tests working

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -1,5 +1,3 @@
-# import logging
-# import os
 import unittest
 import pytest
 
@@ -7,16 +5,17 @@ from lsst.ts.scheduler import Model
 
 class TestSchedulerModel(unittest.TestCase):
 
-    def test_initialize_with_default(self):
+    def test_init(self):
         m = Model()
+        
         assert m.current_state == "OFFLINE"
         assert m.previous_state == "OFFLINE"
         assert m.configuration_path != None
         assert m.valid_settings != None
 
     def test_change_state(self):
-
         m = Model()
+        
         m.current_state = "STANDBY"
         assert m.current_state == "STANDBY"
         assert m.previous_state == "OFFLINE"
@@ -29,11 +28,10 @@ class TestSchedulerModel(unittest.TestCase):
             m.previous_state = "ENABLED"
 
     def test_send_valid_settings(self):
-
         m = Model()
-
+        
         m.sal_start()
-        m.send_valid_settings()
+        assert m.send_valid_settings().find('master') != -1
 
 
 if __name__ == '__main__':
