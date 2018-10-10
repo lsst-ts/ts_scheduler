26f7962e67fcdb8733e14b6b1f502d5b0aa5f04a
Andrew Heyer <aheyer@lsst.org>
2018-10-10 12:23:00 -0700

    docstring update and adding for methods

--- python/lsst/ts/scheduler/model.py
+++ python/lsst/ts/scheduler/model.py
@@ -15,27 +15,43 @@ except ImportError:
 DEFAULT_STATE = "OFFLINE"
 
 class Model():
-    """A class that contains that contains the necessary attributes and methods
-    for a statemachine to control the Scheduling algorith.
+    """Model contains the attributes and methods to control the Scheduling algorithm.
 
-    Parameters
-    ----------
-    driver : ``module``
-        Optional driver for the Model to use. This allows external developers
-        to plug in thier own scheduling algorithms. External Driver's must
-        comply with the the Driver interface here 
-        https://docs.google.com/document/d/1wQ467QGqVP2e4iXOKcW2slLMgOzI_1Qk284b1gDGqaI
+    Attributes:
+        _current_state (str): Current state of the model.
+        _previous_state (str): Previous state of the model, needed to determine
+            actions that require knoewledge of the state the model trasnitions
+            out of.
+        log (logging.Logger): Logger for the Scheduler Model.
+        sal (SALPY_scheduler.SAL_scheduler.SALUtils): Utility class for
+            middleware communication capabilities.
+        configuration_path (str): Directory path for configuration files.
+        configuration_repo (str): Comma delimmited string of available branches
+            for configuring. This contains the full branch name.
+        current_setting (str): Current setting, defaults to the active branch on
+            the configuration_repo.
+        valid_settings (str): Comma delimmited string of available branches
+            for configuring. This contains the short branch name. For example
+            'master' rather than 'origin/master'.
 
-    Notes
-    -----
-    The Model is the highest level interface for the State Machine to interact
-    with. The Scheduler State Machine has multiple states. Rather than having
-    to import many objects from different locations we import this Model object.
-    Allowing the Scheduler State Machine one unified object to make all its
-    calls from.
     """
-
     def __init__(self, driver = Driver()):
+        """The Model object is planned to be configurable by a method TODO:
+
+        Note:
+            The Model is the highest level interface for the State Machine to interact
+            with. The Scheduler State Machine has multiple states. Rather than having
+            to import many objects from different locations we import this Model object.
+            Allowing the Scheduler State Machine one unified object to make all its
+            calls from.
+
+        Args:
+            driver (:obj:`Driver()`, optional): Externally provided Drivers must
+                comply with the Driver interface here
+                https://docs.google.com/document/d/1wQ467QGqVP2e4iXOKcW2slLMgOzI_1Qk284b1gDGqa.
+                Current Driver can be found in lsst.ts.scheduler.Driver
+
+        """
         self._current_state = DEFAULT_STATE
         self._previous_state = DEFAULT_STATE
 
@@ -47,10 +63,24 @@ class Model():
         self.valid_settings = self.read_valid_settings()
 
     def sal_start(self):
+        """Initializes SAL communication.
+
+        Note:
+            SAL communication is not initialized within our init function so
+            that we may test various methods that do not require SAL
+            communication.
+
+        """
         self.sal.start()
 
     def read_valid_settings(self):
-        self.current_setting = str(self.configuration_repo.active_branch)
+        """Reads the branches on the configuration repo and preps them.
+
+        Returns:
+            List of branches on the configuration repository. A single branch
+            represents a valid setting.
+
+        """
         remote_branches = []
         for ref in self.configuration_repo.git.branch('-r').split('\n'):
             if 'HEAD' not in ref:
@@ -60,19 +90,36 @@ class Model():
 
     @property
     def current_state(self):
+        """str: Represents the current state of the model.
+
+        When changed the previous_state becomes the current state.
+        """
         return self._current_state
 
     @current_state.setter
     def current_state(self, state):
-        # TODO: Some actual handing on the state changing mechanism. 
+        # TODO: Some actual handing on the state changing mechanism.
         self._previous_state = self._current_state
         self._current_state = state
 
     @property
     def previous_state(self):
+        """str: Represents the previous state of the model.
+
+        Before current_state is updated, the previous_state becomes the
+        current_state.
+        """
         return self._previous_state
 
     def send_valid_settings(self):
+        """Sends the valid settings over SAL.
+
+        Returns:
+            Comma delimmited string of available branches for configuring.
+            This contains the short branch name. For example 'master' rather
+            than 'origin/master'.
+
+        """
         valid_settings = ''
         for setting in self.valid_settings[:-1]:
             valid_settings += setting[setting.find('/') + 1:]
