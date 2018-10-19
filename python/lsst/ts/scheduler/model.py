c34a9c424c676a9173118edc6ecab77158d26cd8
Tiago Ribeiro <tiago.astro@gmail.com>
2018-10-18 19:38:32 -0700

    Some small teaks to the scheduler model and add some placeholders for methods.

--- python/lsst/ts/scheduler/model.py
+++ python/lsst/ts/scheduler/model.py
@@ -1,10 +1,17 @@
 import time
 import logging
 
-from lsst.ts.scheduler import Driver
 from lsst.ts.scheduler.sal_utils import SALUtils
+from lsst.ts.scheduler.setup import WORDY
+from lsst.ts.dateloc import ObservatoryLocation
+from lsst.ts.observatory.model import ObservatoryModel
+from lsst.ts.observatory.model import ObservatoryState
+from lsst.ts.astrosky.model import AstronomicalSkyModel
 
-from SALPY_scheduler import SAL_scheduler, scheduler_logevent_validSettingsC
+from lsst.sims.seeingModel import SeeingModel
+from lsst.sims.cloudModel import CloudModel
+from lsst.sims.downtimeModel import ScheduledDowntime, UnscheduledDowntime
+
 from scheduler_config.constants import CONFIG_DIRECTORY, CONFIG_DIRECTORY_PATH
 
 try:
@@ -14,7 +21,10 @@ except ImportError:
 
 DEFAULT_STATE = "OFFLINE"
 
-class Model():
+__all__ = ['Model']
+
+
+class Model:
     """Model contains the attributes and methods to control the Scheduling algorithm.
 
     Attributes:
@@ -35,34 +45,33 @@ class Model():
             'master' rather than 'origin/master'.
 
     """
-    def __init__(self, driver = Driver()):
+    def __init__(self):
         """The Model object is planned to be configurable by a method TODO:
 
-        Note:
             The Model is the highest level interface for the State Machine to interact
             with. The Scheduler State Machine has multiple states. Rather than having
             to import many objects from different locations we import this Model object.
             Allowing the Scheduler State Machine one unified object to make all its
             calls from.
 
-        Args:
-            driver (:obj:`Driver()`, optional): Externally provided Drivers must
-                comply with the Driver interface here
-                https://docs.google.com/document/d/1wQ467QGqVP2e4iXOKcW2slLMgOzI_1Qk284b1gDGqa.
-                Current Driver can be found in lsst.ts.scheduler.Driver
-
         """
         self._current_state = DEFAULT_STATE
         self._previous_state = DEFAULT_STATE
 
         self.log = logging.getLogger("schedulerModel")
-        self.sal = SALUtils(2000) #FIXME options arg removed from init, hardcoded value to be set with method in future release.
+        # FIXME options arg removed from init, hardcoded value to be set with method in future release.
+        self.sal = SALUtils(2000)  # FIXME: This is probably going away with salobj
         self.configuration_path = str(CONFIG_DIRECTORY)
         self.configuration_repo = Repo(str(CONFIG_DIRECTORY_PATH))
         self.current_setting = str(self.configuration_repo.active_branch)
         self.valid_settings = self.read_valid_settings()
 
-    def sal_start(self):
+        self.models = {}  # Dictionary to host all the available models.
+        self.raw_telemetry = {}  # Dictionary to host all required telemetry from the models.
+
+        self.driver = None  # This will be setup during the configuration procedure
+
+    def sal_start(self):  # FIXME: This is probably something that should go on the CSC
         """Initializes SAL communication.
 
         Note:
@@ -112,12 +121,12 @@ class Model():
         return self._previous_state
 
     def send_valid_settings(self):
-        """Sends the valid settings over SAL.
+        """Publish valid settings over SAL and return a string with a list of valid settings.
 
-        Returns:
-            Comma delimmited string of available branches for configuring.
-            This contains the short branch name. For example 'master' rather
-            than 'origin/master'.
+        Returns
+        -------
+            valid_settings: string: Comma delimited string of available branches for configuring.
+                This contains the short branch name. For example 'master' rather than 'origin/master'.
 
         """
         valid_settings = ''
@@ -130,3 +139,105 @@ class Model():
         self.sal.logEvent_validSettings(self.sal.topicValidSettings, 5)
 
         return valid_settings
+
+    def init_models(self):
+        """Initialize scheduler models. They will be initialized but not configured.
+
+        Returns
+        -------
+
+        """
+        self.models['location'] = ObservatoryLocation()
+        self.models['observatory_model'] = ObservatoryModel(self.models['location'], WORDY)
+        self.models['observatory_state'] = ObservatoryState()
+        self.models['sky'] = AstronomicalSkyModel(self.models['location'])
+        self.models['seeing'] = SeeingModel()
+        # FIXME: I'll leave cloud model out for now as we need to flush out the cloud model.
+        # self.models['cloud'] = CloudModel()
+        self.models['scheduled_downtime'] = ScheduledDowntime()
+        self.models['unscheduled_downtime'] = UnscheduledDowntime()
+
+        # Fixme: The list of raw telemetry should be something that the models return, plus some additional standard
+        # telemetry like time.
+        # Observatory Time. This is NOT observation time. Observation time will be derived from observatory time by
+        # the scheduler and will aways be in the future.
+        self.raw_telemetry['timeHandler'] = None
+        self.raw_telemetry['observatoryState'] = None  # Observatory State
+        self.raw_telemetry['bulkCloud'] = None  # Transparency measurement
+        self.raw_telemetry['seeing'] = None  # Seeing measurement
+
+    def update_telemetry(self):
+        """ Update data on all the telemetry values.
+
+        Returns
+        -------
+        None
+
+        """
+        pass
+
+    def put_on_queue(self, targets, position=None):
+        """ Given a list of targets, put them on the queue to be observed. By default targets are appended to the
+        queue. An optional position argument is available and specify the position on the queue. Position can either
+        be a single integer number or a list. If an integer, the position is considered to be for the first target on
+        the list. If a list, it must have the same number of elements as targets.
+
+        Parameters
+        ----------
+        targets: list(Targets): A list of targets to put on the queue.
+        position: int or list(int): Position of the targets on the queue. By default (None) append to the queue.
+
+        Returns
+        -------
+
+        """
+        pass
+
+    def configure(self, setting):
+        """This method is responsible for configuring the scheduler models and the scheduler algorithm, given the
+        input setting. It will raise an exception if the input setting is not valid.
+
+        Parameters
+        ----------
+        setting: string: A valid setting from the the `read_valid_settings` method.
+
+        Returns
+        -------
+        None
+
+        """
+        pass
+
+    def configure_driver(self):
+        """Load driver for selected scheduler and configure its basic parameters.
+
+        Returns
+        -------
+
+        """
+        pass
+
+    def configure_scheduler(self):
+        """Configure driver scheduler and publish survey topology.
+
+        Returns
+        -------
+
+        """
+        survey_topology = self.driver.configure_scheduler(config=self.config,
+                                                          config_path=self.configuration_path)
+
+        # FIXME: use salobj to publish survey topology
+        # self.sal.topic_schedulerTopology = survey_topology.to_topic()
+        # self.sal.putSample_surveyTopology(self.sal.topic_schedulerTopology)
+
+    def run(self):
+        """ This is the method that runs when the system is in enable state. It is responsible for the target
+        production loop, updating telemetry, requesting targets from the driver to build a queue and filling
+        the queue with targets.
+
+        Returns
+        -------
+
+        """
+        pass
--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -33,6 +33,28 @@ class TestSchedulerModel(unittest.TestCase):
         m.sal_start()
         assert m.send_valid_settings().find('master') != -1
 
+    def test_init_models(self):
+        """
+        This unit test will check that the basic models are initialized by the model.
+
+        Returns
+        -------
+
+        """
+        models_list = ['location', 'observatory_model', 'observatory_state', 'sky', 'seeing',
+                       'scheduled_downtime', 'unscheduled_downtime']  # 'cloud',
+        telemetry_list = ['timeHandler', 'observatoryState', 'bulkCloud', 'seeing']
+
+        m = Model()
+        m.init_models()
+
+        for model in models_list:
+            assert model in m.models
+            assert m.models[model] is not None
+
+        for telemetry in telemetry_list:
+            assert telemetry in m.raw_telemetry
+
 
 if __name__ == '__main__':
     unittest.main()
