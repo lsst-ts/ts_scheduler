9bd503ae33d66790c244a754d6d4fed95dddfa59
Tiago Ribeiro <tribeiro@lsst.org>
2021-10-07 19:55:27 -0700

    In check_scheduled, only register targets not running in ADVANCE mode. The ADVANCE mode takes cares of that differently.

--- python/lsst/ts/scheduler/scheduler_csc.py
+++ python/lsst/ts/scheduler/scheduler_csc.py
@@ -943,8 +943,8 @@ class SchedulerCSC(salobj.ConfigurableCsc):
                 # use scriptState from info as a first guess
                 # FIXME: we probably need to get updated information about the
                 # observed target
-                self.driver.register_observation([target])
-
+                if self.parameters.mode != "ADVANCE":
+                    self.driver.register_observation([target])
                 # Remove related script from the list
                 del self.script_info[target.sal_index]
                 # target now simply disappears... Should I keep it in for
