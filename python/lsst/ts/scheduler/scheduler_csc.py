c91df6d1eba4a4a9fe5f2e93b540b2d5cdd26210
tribeiro <tribeiro@lsst.org>
2022-06-24 08:56:48 -0700

    In SchedulerCSC.init_models, set sky brightness pre-computed load lenght to 7 days.

--- python/lsst/ts/scheduler/scheduler_csc.py
+++ python/lsst/ts/scheduler/scheduler_csc.py
@@ -695,6 +695,7 @@ class SchedulerCSC(salobj.ConfigurableCsc):
         )
         self.models["observatory_state"] = ObservatoryState()
         self.models["sky"] = AstronomicalSkyModel(self.models["location"])
+        self.models["sky"].sky_brightness_pre.load_length = 7
         self.models["seeing"] = SeeingModel()
         self.models["cloud"] = CloudModel()
         self.models["downtime"] = DowntimeModel()
