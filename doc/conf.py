f5eee8b15a1f188062d3ecba5d5aebeedbb1ce15
Tiago Ribeiro <tribeiro@lsst.org>
2022-08-02 12:26:00 -0700

    Update doc/conf.py to ignore style checks.

--- doc/conf.py
+++ doc/conf.py
@@ -2,16 +2,16 @@
 This configuration only affects single-package Sphinx documentation builds.
 """
 
-from documenteer.conf.pipelinespkg import *
-import lsst.ts.scheduler
+from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
+import lsst.ts.scheduler  # noqa
 
 project = "ts_scheduler"
-html_theme_options["logotext"] = project
+html_theme_options["logotext"] = project  # type: ignore # noqa
 html_title = project
 html_short_title = project
 doxylink = {}  # Avoid warning: Could not find tag file _doxygen/doxygen.tag
 
-intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)
-intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)
-intersphinx_mapping["ts_scriptqueue"] = ("https://ts-scriptqueue.lsst.io", None)
-intersphinx_mapping["rubin_sim"] = ("https://rubin-sim.lsst.io", None)
+intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # type: ignore # noqa
+intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # type: ignore # noqa
+intersphinx_mapping["ts_scriptqueue"] = ("https://ts-scriptqueue.lsst.io", None)  # type: ignore # noqa
+intersphinx_mapping["rubin_sim"] = ("https://rubin-sim.lsst.io", None)  # type: ignore # noqa
