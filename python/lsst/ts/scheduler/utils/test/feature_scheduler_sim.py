5e323848776940bc060f12ad06c2668612250f46
Tiago Ribeiro <tribeiro@lsst.org>
2023-01-27 09:07:37 -0700

    In ``utils/test/feature_scheduler_sim.py``, upgrade import statements to rubin-sim 1.

--- python/lsst/ts/scheduler/utils/test/feature_scheduler_sim.py
+++ python/lsst/ts/scheduler/utils/test/feature_scheduler_sim.py
@@ -28,8 +28,8 @@ from astropy.time import Time
 from lsst.ts.astrosky.model import AstronomicalSkyModel
 from lsst.ts.dateloc import ObservatoryLocation
 from lsst.ts.observatory.model import ObservatoryModel, ObservatoryState
-from rubin_sim.site_models.cloudModel import CloudModel
-from rubin_sim.site_models.seeingModel import SeeingModel
+from rubin_sim.site_models.cloud_model import CloudModel
+from rubin_sim.site_models.seeing_model import SeeingModel
 
 from ...driver import FeatureScheduler
 from ...driver.feature_scheduler_target import FeatureSchedulerTarget
