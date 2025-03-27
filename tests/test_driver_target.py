e3da6ae11d76893d1fbe84a6bc1a652948de7022
Tiago Ribeiro <tribeiro@lsst.org>
2025-03-27 09:10:08 -0700

    Update the DriverTarget unit tests to include the new fields added to the script configuration.

--- tests/test_driver_target.py
+++ tests/test_driver_target.py
@@ -147,7 +147,9 @@ class TestDriverTarget(unittest.TestCase):
         script_config_expected = {
             "targetid": target.targetid,
             "band_filter": target.filter,
-            "name": target.note,
+            "filter_name": target.get_filter_name(),
+            "name": target.get_target_name(),
+            "note": target.note,
             "ra": target.get_ra(),
             "dec": target.get_dec(),
             "rot_sky": target.ang,
