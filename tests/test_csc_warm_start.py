6125c2c0d8a2422d1cff2adb9880e31a5255593b
Tiago Ribeiro <tiago.ribeiro@noirlab.edu>
2025-12-16 14:13:13 -0700

    Update unit tests to handle storing the FBS state adding the additional value for the queued targets.

--- tests/test_csc_warm_start.py
+++ tests/test_csc_warm_start.py
@@ -86,6 +86,7 @@ class TestSchedulerCscWarmStart(
                     [
                         conf.scheduler,
                         conditions,
+                        [],
                     ],
                     fp,
                 )
