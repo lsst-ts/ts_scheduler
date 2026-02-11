7cf7168568a25c5632b5b12169c73d244c29a705
Tiago Ribeiro <tiago.ribeiro@noirlab.edu>
2026-02-11 14:49:18 -0700

    In ``utils/__init__.py``, remove import of exceptions module.

--- python/lsst/ts/scheduler/utils/__init__.py
+++ python/lsst/ts/scheduler/utils/__init__.py
@@ -19,7 +19,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from ..exceptions.exceptions import *
 from .csc_utils import *
 from .error_codes import *
 from .fbs_utils import *
