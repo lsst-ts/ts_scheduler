c00a8834bbda79cca7cbe2607b6b811985cd0032
Tiago Ribeiro <tiago.ribeiro@noirlab.edu>
2026-07-05 12:24:13 -0400

    In ``model.py``, replace call to deprecated method ``copy`` from Pydandic with ``model_copy``.

--- python/lsst/ts/scheduler/model.py
+++ python/lsst/ts/scheduler/model.py
@@ -357,7 +357,7 @@ class Model:
 
         for block_id in self.observing_blocks:
             target_validate = DriverTarget(
-                observing_block=self.observing_blocks[block_id].copy(deep=True)
+                observing_block=self.observing_blocks[block_id].model_copy(deep=True)
             )
             observing_block = target_validate.get_observing_block()
             for script in observing_block.scripts:
